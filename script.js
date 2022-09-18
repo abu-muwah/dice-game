07b171fbb6df281612cd984196245eb474edc511
ABU_MUWAH <abu.muwah@gmail.com>
2022-09-18 10:54:03 +0430

    Adding variables

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict';
 
+const score0 = document.getElementById('score--0')
+const score1 = document.getElementById('score--1')
+const dice = document.querySelector('.dice')
+const rollBtn = document.querySelector('.btn--roll')
+const holdBtn = document.querySelector('.btn--hold')
+const newBtn = document.querySelector('.btn--new')
+
+
+score0.textContent = 0
+score1.textContent = 0
+dice.classList.add('hidden')
+
+
 
